9a2460af5a4f5589c4d10fe271aad4424edde59d
Treasure <treasurewmj@foxmail.com>
2018-03-30 17:05:29 +0800

    唬住面试官

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-class Animal {
-  
+let arr = []
+for (let i = 1; i < 101; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    arr.push('FizzBuzz')
+  } else if (i % 3 === 0) {
+    arr.push('Fizz')
+  } else if (i % 5 === 0) {
+    arr.push('Buzz')
+  } else {
+    continue
+  }
 }
+
+console.log(arr)
