000f317dc24c50e9782a87b260b2ff27b5451397
Treasure <treasurewmj@foxmail.com>
2018-03-30 17:05:20 +0800

    唬住面试官

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-const babylon = require('babylon')
-const fs = require('fs-extra')
-
-const opt = {
-  sourceType: 'module'
+class Animal {
+  
 }
-
-async function main() {
-  let script = await fs.readFile('script.js')
-  script = script.toString()
-  const output = babylon.parse(script, opt)
-  fs.writeFile('output.json', JSON.stringify(output))
-}
-
-main()
