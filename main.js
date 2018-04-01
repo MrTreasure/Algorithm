7d76adbeed4fdf332808f4bf5c3dc9784dbbec79
Treasure <treasurewmj@foxmail.com>
2018-04-02 00:22:42 +0800

    集群的任务分布

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-import * as axios from 'axios'
+const axios = require('axios')
+const fs = require('fs-extra')
+const os = require('os')
 
 const map = new Map()
 
+const MAX = 1000
 
+for (let i = 0; i < MAX; i++) {
+  axios.get('http://localhost:2001')
+    .then(res => {
+      const data = res.data
+      if (map.has(data.pid)) {
+        let count = map.get(data.pid)
+        map.set(data.pid, ++count)
+      } else {
+        map.set(data.pid, 0)
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
 
 function map2Obj(map) {
   let obj = Object.create(null)
-  for (let [k,v] in map) {
+  for (let [k,v] of map) {
     obj[k] = v
   }
   return obj
 }
 
-process.on('beforeExit', () => {
+process.on('beforeExit', async () => {
+  let obj = map2Obj(map)
 
+  await fs.appendFile('./cluster.txt', JSON.stringify(obj) + os.EOL)
+  process.exit(0)
 })
--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-import fs from 'fs-extra'
+const axios = require('axios')
+const fs = require('fs-extra')
 
-console.log('Hello World')
+const map = new Map()
 
-// document here
+const MAX = 1000
 
-class Hello {
-  constructor() {
-    this.name = 'first'
-  }
+for (let i = 0; i < MAX; i++) {
+  axios.get('http://localhost:2001')
+    .then(res => {
+      const data = res.data
+      if (map.has(data.pid)) {
+        let count = map.get(data.pid)
+        map.set(data.pid, ++count)
+      } else {
+        map.set(data.pid, 0)
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
+
+function map2Obj(map) {
+  let obj = Object.create(null)
+  for (let [k,v] of map) {
+    obj[k] = v
+  }
+  return obj
+}
+
+process.on('exit', async () => {
+  let obj = map2Obj(map)
+  let str = JSON.stringify(obj)
+  console.log(str)
+  await fs.writeJson('./cluster.json', obj)
+})
