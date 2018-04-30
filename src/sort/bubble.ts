4312813331bb49b94eec6530127bfc4cdf2ec811
Treasure <treasurewmj@foxmail.com>
2018-04-30 23:00:12 +0800

    冒泡排序条件少减少一次循环

--- src/sort/bubble.ts
+++ src/sort/bubble.ts
@@ -1,8 +1,8 @@
 import { produceNums } from './nums'
 // 冒泡排序时间复杂度O(n^2)
 function bubble (arr: number[]): number[] {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - 1 - i; j++) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j + 1]
         arr[j + 1] = arr[j]
