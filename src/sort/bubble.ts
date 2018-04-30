ecedc5d6938f0f0548dc243790d51c694bcea3f9
Treasure <treasurewmj@foxmail.com>
2018-04-30 23:05:27 +0800

    -1是因为不需要把下标移动到最后一个

--- src/sort/bubble.ts
+++ src/sort/bubble.ts
@@ -1,8 +1,9 @@
 import { produceNums } from './nums'
 // 冒泡排序时间复杂度O(n^2)
 function bubble (arr: number[]): number[] {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i; j++) {
+  // -1是因为不需要把下标移动到最后一个
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j + 1]
         arr[j + 1] = arr[j]
