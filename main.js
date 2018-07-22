98008456034b013932c864a562d9f6447aa8be5e
Treasure <treasurewmj@foxmail.com>
2018-07-22 18:05:26 +0800

    ES5 ES6对象遍历的差异

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-const obj = {
-  name: 'Treasure',
-  say() {
-    console.log(this.name)
-  }
+const chalk = require('chalk')
+function A() {
+  this.name = 'Treasure'
 }
 
+A.prototype.say = function() {
+  console.log(this.name)
+}
 
-class A {
+const a = new A()
+
+function B () {
+  this.age = 22
+}
+
+B.prototype = new A()
+B.prototype.run = function () {
+  console.log('run')
+}
+
+class classA {
   constructor() {
     this.name = 'Treasure'
   }
@@ -16,27 +28,35 @@ class A {
   }
 }
 
-class B extends A {
+class classB extends classA {
+  constructor(props) {
+    super(props)
+    this.age = 22
+  }
   run() {
-    console.log('run')
+    console.log(run)
   }
 }
 
 const b = new B()
-
-console.log(B.prototype.say)
+const classb = new classB()
 
 const print = (obj) => {
+  const result = []
   for (let key in obj) {
-    console.log(key)
+    result.push(key)
   }
+  console.log(chalk.red(result.join(',')))
 
-  Object.keys(obj).forEach(key => console.log(key))
+  console.log(chalk.green(Object.keys(obj).reduce((prev, next) => { return prev + ',' + next})))
 
-  Object.getOwnPropertyNames(obj).forEach(key => console.log(key))
+  console.log(chalk.yellow(Object.getOwnPropertyNames(obj).reduce((prev, next) => { return prev + ',' + next})))
 
-  Reflect.ownKeys(obj).forEach(key => console.log(key))
+  console.log(chalk.blue(Reflect.ownKeys(obj).reduce((prev, next) => { return prev + ',' + next})))
 }
 
+console.log('ES5 -> B')
 print(b)
-console.log(b.name)
+
+console.log('ES6 -> B')
+print(classb)
