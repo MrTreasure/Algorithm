17296fac4d2f9277b8d28a45887890c11e4d720d
Treasure <treasurewmj@foxmail.com>
2018-08-01 19:47:02 +0800

    新的文章

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
-function observe (obj) {
-  if (obj.__ob__) {
-    return obj
-  } else {
-    return new Proxy(obj, {
-      get (target, key) {
-        if (typeof target[key] === 'object' && !target.__ob__) {
-          target[key] = observe(target[key])
-          target.__ob__ = true
-        }
-        console.log(`get : ${target[key]}`)
-        return target[key]
-      },
-      set (target, key, val) {
-        console.log(`set ${key} to ${val}`)
-        target[key] = val
-      }
-    })
-  }
-}
 
-const obj = { a: 1 }
+setImmediate(() => {
+  console.log(5)
+})
+setTimeout(() => {
+  console.log('1')
+  Promise.resolve().then(() => {
+    console.log(2)
+  })
+}, 0);
 
-const p = observe(obj)
-p.a
-p.a = 3
+Promise.resolve().then(() => {
+  console.log(3)
+})
+
+process.nextTick(() => {
+  console.log(4)
+})
